2233be6dde386a5d9654690d8c7687b28837bb85
Thiago Artur Schumann <thiagoarturschumann@gmail.com>
2022-10-31 20:14:54 -0300

    update ALLOWED_HOSTS

--- badmint/settings.py
+++ badmint/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'badmint-api-xf2jb.ondigitalocean.app',
-    'https://badmint-back-end-develop-7v2jv.ondigitalocean.app/',
+    'badmint-back-end-develop-7v2jv.ondigitalocean.app',
     '127.0.0.1',
 ]
 
